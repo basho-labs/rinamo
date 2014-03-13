5a09a5cb67624239d1f6811167d70821fcf87116
Randy Secrist <randy.secrist@gmail.com>
2014-03-13 15:14:08 -0600

    update integ test for empty put and delete table

--- tests/com.basho.dynamodb.integ/src/test/java/com/basho/dynamodb/integ/TableOperationsITCase.java
+++ tests/com.basho.dynamodb.integ/src/test/java/com/basho/dynamodb/integ/TableOperationsITCase.java
@@ -2,6 +2,9 @@ package com.basho.dynamodb.integ;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,19 +14,26 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.CreateTableResult;
+import com.amazonaws.services.dynamodbv2.model.DeleteTableResult;
 import com.amazonaws.services.dynamodbv2.model.DescribeTableResult;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class TableOperationsITCase {
 
   private AmazonDynamoDBClient client;
+  
+  private String tableName;
 
   @Before
   public void before() {
@@ -38,15 +48,23 @@ public class TableOperationsITCase {
 
     client = new AmazonDynamoDBClient(creds);
     client.setEndpoint("http://localhost:8098/rinamo");
+//    client.setEndpoint("http://localhost:8000");
+    
+    tableName = "TestTable";
   }
 
   @After
   public void after() {
+    
+  }
+  
+  @Test
+  public void startupTest() {
+    assertTrue(true);
   }
 
   @Test
   public void createTableTest() {
-    String tableName = "TestTable";
     String hashKeyName = "Id", hashKeyType = "N";
     String rangeKeyName = null, rangeKeyType = null;
     long readCapacityUnits = 10L, writeCapacityUnits = 5L;
@@ -86,7 +104,41 @@ public class TableOperationsITCase {
 
   @Test
   public void describeTableTest() {
-    DescribeTableResult result = client.describeTable("ProductCatalog");
+    DescribeTableResult result = client.describeTable(tableName);
     assertNotNull(result);
   }
+  
+  @Test
+  public void putItemTest() {
+    Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+    item.put("Id", new AttributeValue().withN("101"));
+    item.put("Title", new AttributeValue().withS("Book 101 Title"));
+    item.put("ISBN", new AttributeValue().withS("111-1111111111"));
+    item.put("Authors", new AttributeValue().withSS(Arrays.asList("Author1")));
+    item.put("Price", new AttributeValue().withN("2"));
+    item.put("Dimensions", new AttributeValue().withS("8.5 x 11.0 x 0.5"));
+    item.put("PageCount", new AttributeValue().withN("500"));
+    item.put("InPublication", new AttributeValue().withN("1"));
+    item.put("ProductCategory", new AttributeValue().withS("Book"));
+    PutItemRequest pir = new PutItemRequest().withItem(item);
+    pir.setTableName(tableName);
+    PutItemResult result = client.putItem(pir);
+    assertNotNull(result);
+  }
+  
+  @Test
+  public void putItemNoTableTest() {
+    Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+
+    PutItemRequest pir = new PutItemRequest().withItem(item);    
+    PutItemResult result = client.putItem(pir);
+    assertNotNull(result);
+  }
+
+  @Test
+  public void deleteTableTest() {
+    DeleteTableResult result = client.deleteTable(tableName);
+    assertNotNull(result);
+  }
+
 }
