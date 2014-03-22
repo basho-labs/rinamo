a05f44f1cccf341e539634dc29839aec49f25f91
Nathan Aschbacher <nathan@basho.com>
2014-03-22 13:33:57 -0700

    Removed endpoint_prefix functions, added cowboy config functions.

--- src/rinamo_config.erl
+++ src/rinamo_config.erl
@@ -1,12 +1,20 @@
 -module(rinamo_config).
 
 -export([
-          is_enabled/0,
-          endpoint_prefix/0
-    ]).
+    is_enabled/0,
+    get_protocol/0,
+    get_bind_address/0,
+    get_num_acceptors/0
+]).
 
 is_enabled() ->
     app_helper:get_env(rinamo, enabled, true).
 
-endpoint_prefix() ->
-    app_helper:get_env(rinamo, endpoint_prefix, "rinamo").
+get_protocol() ->
+    proplists:get(protocol, app_helper:get_env(rinamo, network)).
+
+get_bind_address() ->
+    proplists:get(bind, app_helper:get_env(rinamo, network)).
+
+get_num_acceptors() ->
+    proplists:get(acceptors, app_helper:get_env(rinamo, network)).
