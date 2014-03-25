876a2bd5d224be328177ce30e939c712dba0b095
Nathan Aschbacher <nathan@basho.com>
2014-03-25 07:27:27 -0700

    Converted to raw http_handler rather than rest_handler.  Because the REST protocol is too opinionated and heavyweight for this application.

--- src/rinamo_handler_ping.erl
+++ src/rinamo_handler_ping.erl
@@ -1,21 +1,18 @@
 -module(rinamo_handler_ping).
 
+-behaviour(cowboy_http_handler).
+
 -export([
     init/3,
-    allowed_methods/2,
-    content_types_provided/2
+    handle/2,
+    terminate/3
 ]).
 
-init(_Type, _Req, _Opts) ->
-    {upgrade, protocol, cowboy_rest}.
+init(_Type, Req, _Opts) ->
+    {ok, Req, undefined}.
 
-allowed_methods(Req, State) ->
-    {[<<"GET">>, <<"HEAD">>], Req, State}.
+handle(Req, State) ->
+    {ok, Req, State}.
 
-content_types_provided(Req, State) ->
-    {[
-        {<<"text/plain">>, ping_to_text}
-    ], Req, State}.
-
-ping_to_text(Req, State) ->
-    {<<"OK">>, Req, State}.
+terminate(_Reason, _Req, _State) ->
+    ok.
