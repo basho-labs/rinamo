c887a04448ed3a8c6dadf4d9ea3f861698291b83
Nathan Aschbacher <nathan@basho.com>
2014-03-25 07:28:05 -0700

    Added modularized error creation and formatting.

--- include/rinamo.hrl
+++ include/rinamo.hrl
@@ -2,5 +2,13 @@
 	user_key :: binary()
 }).
 
+-record(error, {
+    http_code :: integer(),
+    code :: binary(),
+    message :: binary()
+}).
 
 -define(RINAMO_SEPARATOR, 16#1f).
+
+-define(AMZ_AUTH_HEADER,<<"authorization">>).
+-define(AMZ_OP_HEADER,<<"x-amz-target">>).
