5a052b9b16cf77622e269eba4b23d159e15cb3ee
Randy Secrist <randy.secrist@gmail.com>
2014-03-25 18:14:43 -0600

    fix func call to get things

--- src/rinamo_config.erl
+++ src/rinamo_config.erl
@@ -11,10 +11,10 @@ is_enabled() ->
     app_helper:get_env(rinamo, enabled, true).
 
 get_protocol() ->
-    proplists:get(protocol, app_helper:get_env(rinamo, network)).
+    proplists:get_value(protocol, app_helper:get_env(rinamo, network)).
 
 get_bind_address() ->
-    proplists:get(bind, app_helper:get_env(rinamo, network)).
+    proplists:get_value(bind, app_helper:get_env(rinamo, network)).
 
 get_num_acceptors() ->
-    proplists:get(acceptors, app_helper:get_env(rinamo, network)).
+    proplists:get_value(acceptors, app_helper:get_env(rinamo, network)).
