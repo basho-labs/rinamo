86dffb4a78277beab8771c2580fc14e1803ee5bf
Nathan Aschbacher <nathan@basho.com>
2014-03-27 10:35:29 -0700

    Added tests for request id middleware

--- src/rinamo_middleware_reqid.erl
+++ src/rinamo_middleware_reqid.erl
@@ -1,16 +1,11 @@
 -module(rinamo_middleware_reqid).
-
 -behaviour(cowboy_middleware).
-
 -export([execute/2]).
 
 -include("rinamo.hrl").
 
 -define(AMZ_REQ_ID_HEADER,<<"x-amzn-requestid">>).
 
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
--endif.
 
 execute(Req, Env) ->
     TaggedReq = cowboy_req:set_resp_header(?AMZ_REQ_ID_HEADER, create_request_id(), Req),
@@ -18,3 +13,14 @@ execute(Req, Env) ->
 
 create_request_id() ->
     uuid:get_v5_compat(<<"rinamo to rule them all">>).
+
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+create_request_id_test() ->
+    ?assert(create_request_id()),
+    ?assert(byte_size(create_request_id()) >= 0).
+
+-endif.
