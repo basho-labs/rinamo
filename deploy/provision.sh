9a37a256d0a7c84e2d0a2378d4cd4f473b12607f
Randy Secrist <randy.secrist@gmail.com>
2014-04-15 15:38:11 -0600

    until plergins, always rebuild the release

--- deploy/provision.sh
+++ deploy/provision.sh
@@ -19,21 +19,26 @@ if [ ! -d riak ]; then
   su --preserve-environment -c "git clone -b rs-rinamo-vagrant git@github.com:basho/riak.git" vagrant
   pushd riak
   su -c "make locked-all rel" vagrant
-  pushd rel/riak
-  su -c "sed -i.bak 's/anti_entropy = active/anti_entropy = passive/' etc/riak.conf" vagrant
-  su -c "sed -i.bak 's/log.console.level = info/log.console.level = debug/' etc/riak.conf" vagrant
-  su -c "sed -i.bak 's/listener.http.internal = 127.0.0.1:8098/listener.http.internal = 0.0.0.0:8098/' etc/riak.conf" vagrant
-  su -c "echo 'rinamo.network.bind = 0.0.0.0:8000' >> etc/riak.conf" vagrant
-
   popd
+else
+  # rebuild rinamo release
+  pushd riak
+  su -c "rel/riak/bin/riak stop; rm -rf rel/riak; rm -rf deps/rinamo; make locked-all rel" vagrant
   popd
 fi
 
-# Start Riak
+# Post Rel Configure
 pushd riak/rel/riak
+su -c "sed -i.bak 's/anti_entropy = active/anti_entropy = passive/' etc/riak.conf" vagrant
+su -c "sed -i.bak 's/log.console.level = info/log.console.level = debug/' etc/riak.conf" vagrant
+su -c "sed -i.bak 's/listener.http.internal = 127.0.0.1:8098/listener.http.internal = 0.0.0.0:8098/' etc/riak.conf" vagrant
+su -c "sed -i.bak 's/rinamo.network.bind = 127.0.0.1:8000/rinamo.network.bind = 0.0.0.0:8000/' etc/riak.conf" vagrant
+
+# Start Riak
 ulimit -n 32768
 su -c "./bin/riak start" vagrant
 su -c "./bin/riak ping" vagrant
+popd
 
 echo
 echo "--"
