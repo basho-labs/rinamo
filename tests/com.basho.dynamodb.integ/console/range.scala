84a776d14eca7bcccbd68f00090f329c8306a9be
Randy Secrist <randy.secrist@gmail.com>
2014-05-01 10:32:13 -0600

    minor updates to test key conditions

--- tests/com.basho.dynamodb.integ/console/range.scala
+++ tests/com.basho.dynamodb.integ/console/range.scala
@@ -3,14 +3,17 @@ Table.create("books_range", "Id", "N", Some("Title"), Some("S"))
 val i1 = new Item()
 i1.add("Id", "N", "101")
 i1.add("Title", "S", "Some Title")
+i1.add("ISBN", "S", "ABC")
 
 val i2 = new Item()
 i2.add("Id", "N", "102")
 i2.add("Title", "S", "Another Title")
+i2.add("ISBN", "S", "DEF")
 
 val i3 = new Item()
 i3.add("Id", "N", "101")
 i3.add("Title", "S", "Tale of Two Databases")
+i3.add("ISBN", "S", "XYZ")
 
 Table.put("books_range", i1)
 Table.put("books_range", i2)
