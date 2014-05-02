cf5d40ce796f0b8a97b61d166d4bd2d634e99a94
Randy Secrist <randy.secrist@gmail.com>
2014-05-02 17:19:44 -0600

    add test case for item proxy strat

--- src/rinamo_items.erl
+++ src/rinamo_items.erl
@@ -242,7 +242,7 @@ map_key_condition_test() ->
 
     meck:unload([rinamo_tables, rinamo_kv]).
 
-query_test() ->
+query_one_one_test() ->
     meck:new([rinamo_config, rinamo_tables, rinamo_kv, rinamo_set], [non_strict]),
     meck:expect(rinamo_config, get_index_strategy, 0, rinamo_idx_one_for_one),
     meck:expect(rinamo_set, client, 0, ok),
@@ -274,5 +274,41 @@ query_test() ->
     ?assertEqual(Expected, Actual),
 
     meck:unload([rinamo_config, rinamo_tables, rinamo_kv, rinamo_set]).
+
+query_item_proxy_test() ->
+    meck:new([rinamo_config, rinamo_tables, rinamo_set], [non_strict]),
+    meck:expect(rinamo_config, get_index_strategy, 0, rinamo_idx_item_proxies),
+    meck:expect(rinamo_set, client, 0, ok),
+    meck:expect(rinamo_set, value, 3, {value,
+        [{<<"Book 101">>,
+         [{<<"Id">>,[{<<"N">>,<<"101">>}]},
+          {<<"Title">>,[{<<"S">>,<<"Book 101">>}]},
+          {<<"ISBN">>,[{<<"S">>,<<"ABC">>}]}]},
+        {<<"Book 102">>,
+         [{<<"Id">>,[{<<"N">>,<<"102">>}]},
+          {<<"Title">>,[{<<"S">>,<<"Book 102">>}]},
+          {<<"ISBN">>,[{<<"S">>,<<"XYZ">>}]}]}]
+    }),
+    meck:expect(rinamo_tables, load_table_def, 2, jsx:decode(range_table_fixture())),
+
+    Table = <<"TableName">>,
+    KeyConditions = [
+        {<<"Title">>,[{<<"S">>,<<"Book 101">>}],<<"BEGINS_WITH">>},
+        {<<"ISBN">>, [{<<"S">>,<<"ABC">>}],<<"EQ">>},
+        {<<"Id">>,[{<<"N">>,<<"101">>}],<<"EQ">>}],
+    AWSContext=#state{ user_key = <<"TEST_API_KEY">> },
+
+    Actual = query(Table, KeyConditions, AWSContext),
+
+    Expected = [[
+        {<<"Id">>,[{<<"N">>,<<"101">>}]},
+        {<<"Title">>,[{<<"S">>,<<"Book 101">>}]},
+        {<<"ISBN">>,[{<<"S">>,<<"ABC">>}]}
+    ]],
+
+    ?assertEqual(Expected, Actual),
+
+    meck:unload([rinamo_config, rinamo_tables, rinamo_set]).
+
 
 -endif.
