06f13bc47542ee54629e8a6a3be5baf2962ffff5
Randy Secrist <randy.secrist@gmail.com>
2014-05-05 22:06:27 -0600

    use await instead of Thread.sleep

--- tests/com.basho.dynamodb.integ/src/test/scala/acceptance/HashKeyTest.scala
+++ tests/com.basho.dynamodb.integ/src/test/scala/acceptance/HashKeyTest.scala
@@ -1,6 +1,5 @@
 package acceptance
 
-import scala.beans.BeanProperty
 import org.scalatest.BeforeAndAfterEach
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
@@ -8,34 +7,58 @@ import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
 import org.mockito.Matchers._
 
-import com.amazonaws.services.dynamodbv2.model._
+import java.util.concurrent.TimeUnit._
 
-class HashKeyTest extends FunSpec with AWSHelper with MockitoSugar with Matchers with BeforeAndAfterEach {
-  val table_name = "books_hash"
+import com.amazonaws.services.dynamodbv2.model._
+import com.jayway.awaitility.scala._
+import com.jayway.awaitility.Awaitility._
+
+class HashKeyTest extends FunSpec
+  with AWSHelper with MockitoSugar with Matchers
+  with BeforeAndAfterEach with AwaitilitySupport {
+
+  val table_name = "test_table_hash"
   var table:CreateTableResult = null
 
-  // todo:  better test idempotence
-  // (using poor man's sleep for now)
   override def beforeEach() {
     table = Table.create(table_name, "Id", "N")
-    Thread.sleep(5)
+    await atMost(500, MILLISECONDS) until {
+      val result = Table.describe(table_name)
+      table_name.equals(result.getTable().getTableName())
+    }
   }
   override def afterEach() {
     try {
-      Table.delete("books_hash")
-      Thread.sleep(5)
+      Table.delete(table_name)
     }
-    finally {}
+    catch {
+      case e: ResourceNotFoundException => {}
+    }
+    await atMost(500, MILLISECONDS) until {
+      var exception:Throwable = null
+      try {
+        Table.describe(table_name)
+      }
+      catch {
+        case e: Throwable => {
+          exception = e
+        }
+      }
+      exception != null
+    }
    }
 
   describe ("test uses dynamodb api, table using hash key") {
-    val table_name = "books_hash"
     describe ("US193704: manage tables, create table") {
       it ("should create table") {
         assert(table_name.equals(table.getTableDescription().getTableName()))
       }
 
-      it ("should fail creating an existing table") (pending)
+      it ("should fail creating an existing table") {
+        evaluating {
+          Table.create(table_name, "Id", "N")
+        } should produce [ResourceInUseException]
+      }
     }
     describe ("US193705: manage tables, describe table") {
       it ("should describe a table") {
@@ -43,21 +66,42 @@ class HashKeyTest extends FunSpec with AWSHelper with MockitoSugar with Matchers
         assert(table_name.equals(describe_result.getTable().getTableName()))
       }
 
-      it ("should fail describing a non existing table") (pending)
+      it ("should fail describing a non existing table") {
+        evaluating {
+          Table.describe("nonexistant_table")
+        } should produce [ResourceNotFoundException]
+      }
     }
     describe ("US193707: manage tables, list tables") {
       it ("should list tables") {
         val table_list = Table.list()
-        assert(table_list find { case (x) => x == "books_hash" } isDefined)
+        assert(table_list find { case (x) => x == table_name } isDefined)
       }
     }
     describe ("US193708: manage tables, delete table") {
-      it ("should delete a table") (pending)
+      it ("should delete a table") {
+        val delete_result = Table.delete(table_name)
+        assert(table_name.equals(delete_result.getTableDescription().getTableName()))
+      }
 
-      it ("should fail deleting a non existing table") (pending)
+      it ("should fail deleting a non existing table") {
+        evaluating {
+          Table.delete("nonexistant_table")
+        } should produce [ResourceNotFoundException]
+      }
     }
     describe ("US193713: modify data, put item") {
+      val item = new Item()
+      item.add("Id", "N", "101")
+      item.add("Title", "S", "Some Title")
+
       it ("should put item in table") (pending)
+
+      it ("should fail put if using non existing table") {
+        evaluating {
+          Table.put("nonexistant_table", item)
+        } should produce [ResourceNotFoundException]
+      }
     }
     describe ("US193709: read data, get item") {
       it ("should read item from table") (pending)
