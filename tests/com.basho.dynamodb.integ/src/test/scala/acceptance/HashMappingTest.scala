626c53a86b38d8df813a8a376765cef3ae43217d
Randy Secrist <randy.secrist@gmail.com>
2014-05-09 07:51:23 -0600

    :lipstick: split out query tests

--- tests/com.basho.dynamodb.integ/src/test/scala/acceptance/HashMappingTest.scala
+++ tests/com.basho.dynamodb.integ/src/test/scala/acceptance/HashMappingTest.scala
@@ -14,7 +14,7 @@ import com.amazonaws.services.dynamodbv2.model._
 import com.jayway.awaitility.scala._
 import com.jayway.awaitility.Awaitility._
 
-class HashKeyTest extends FunSpec
+class HashMappingTest extends FunSpec
   with AWSHelper with MockitoSugar with Matchers
   with BeforeAndAfterEach with AwaitilitySupport {
 
@@ -65,6 +65,9 @@ class HashKeyTest extends FunSpec
    }
 
   describe ("test uses dynamodb api, table using hash key") {
+    it ("should create test table") {
+      
+    }
     describe ("US193704: manage tables, create table") {
       it ("should create table") {
         assert(hash_table_name.equals(table_hash.getTableDescription().getTableName()))
@@ -162,93 +165,6 @@ class HashKeyTest extends FunSpec
           assert("Tale of Two Databases".equals(query_result.getItems().get(0).get("Title").getS()))
         }
       }
-    }
-
-    describe ("[US193709, US193711]: table type, hash + range, LWW behavior") {
-      val range_table_name = "test_table_range"
-      it ("should create range table") {
-        val table_range:CreateTableResult = Table.create(range_table_name, "Id", "N", Some("Title"), Some("S"))
-        Table.put(range_table_name)(item_1, item_2, item_3)
-        await atMost(2, MINUTES) until {
-          val result = Table.describe(range_table_name)
-          // TODO:  and these together, once rinamo goes async
-          "ACTIVE".equals(result.getTable().getTableStatus())
-          range_table_name.equals(result.getTable().getTableName())
-        }
-      }
-
-      describe ("read data, query item") {
-        it ("from table using EQ") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("EQ"), Some("Some Title"))
-          assert(query_result.getCount() == 1)
-          assert("Some Title".equals(query_result.getItems().get(0).get("Title").getS()))
-        }
-        it ("from table using LE") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("LE"), Some("Tale of Two Databases"))
-          assert(query_result.getCount() == 2)
-        }
-        it ("from table using LT") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("LT"), Some("Tale of Two Databases"))
-          assert(query_result.getCount() == 1)
-        }
-        it ("from table using GE") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("GE"), Some("Tale of Two Databases"))
-          assert(query_result.getCount() == 1)
-        }
-        it ("from table using GT") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("GT"), Some("Tale of Two Databases"))
-          assert(query_result.getCount() == 0)
-        }
-        it ("from table using BEGINS_WITH") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("BEGINS_WITH"), Some("Some"))
-          assert(query_result.getCount() == 1)
-          assert("Some Title".equals(query_result.getItems().get(0).get("Title").getS()))
-        }
-        it ("from table using BETWEEN") {
-          val query_result = Table.query(
-              range_table_name, "Id", "101",
-              Some("Title"), Some("BETWEEN"), Some("A"), Some("Z"))
-          assert(query_result.getCount() == 2)
-        }
-        it ("finds all values when using hash key only") {
-          val query_result = Table.query(range_table_name, "Id", "101")
-          assert(query_result.getCount() == 2)
-        }
-      }
-
-      it ("should delete range table") {
-        try {
-          Table.delete(range_table_name)
-        }
-        catch {
-          case e: ResourceNotFoundException => {}
-        }
-        await atMost(2, MINUTES) until {
-          var exception:Throwable = null
-          try {
-            Table.describe(range_table_name)
-          }
-          catch {
-            case e: Throwable => {
-              exception = e
-            }
-          }
-          exception != null
-        }
-      }
-
     }
 
     describe ("US193712: read data, scan item") {
---
+++ tests/com.basho.dynamodb.integ/src/test/scala/acceptance/QueryMapping.scala
@@ -0,0 +1,120 @@
+package acceptance
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.FunSpec
+import org.scalatest.Matchers
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+
+import java.util.concurrent.TimeUnit._
+
+import com.amazonaws._
+import com.amazonaws.services.dynamodbv2.model._
+import com.jayway.awaitility.scala._
+import com.jayway.awaitility.Awaitility._
+
+class QueryMappingTest extends FunSpec
+  with AWSHelper with MockitoSugar with Matchers
+  with BeforeAndAfterEach with AwaitilitySupport {
+
+  val item_1 = new Item(
+    ("Id", "N", "101"),
+    ("Title", "S", "Some Title"),
+    ("ISBN", "S", "ABC"))
+  val item_2 = new Item(
+    ("Id", "N", "102"),
+    ("Title", "S", "Another Title"),
+    ("ISBN", "S", "DEF"))
+  val item_3 = new Item(
+    ("Id", "N", "101"),
+    ("Title", "S", "Tale of Two Databases"),
+    ("ISBN", "S", "XYZ"))
+
+    describe ("[US193709, US193711]: table type, hash + range, LWW behavior") {
+      val range_table_name = "test_table_range"
+      it ("should create range table") {
+        val table_range:CreateTableResult = Table.create(range_table_name, "Id", "N", Some("Title"), Some("S"))
+        Table.put(range_table_name)(item_1, item_2, item_3)
+        await atMost(2, MINUTES) until {
+          val result = Table.describe(range_table_name)
+          // TODO:  and these together, once rinamo goes async
+          "ACTIVE".equals(result.getTable().getTableStatus())
+          range_table_name.equals(result.getTable().getTableName())
+        }
+      }
+
+      describe ("read data, query item") {
+        it ("from table using EQ") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("EQ"), Some("Some Title"))
+          assert(query_result.getCount() == 1)
+          assert("Some Title".equals(query_result.getItems().get(0).get("Title").getS()))
+        }
+        it ("from table using LE") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("LE"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 2)
+        }
+        it ("from table using LT") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("LT"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 1)
+        }
+        it ("from table using GE") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("GE"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 1)
+        }
+        it ("from table using GT") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("GT"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 0)
+        }
+        it ("from table using BEGINS_WITH") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("BEGINS_WITH"), Some("Some"))
+          assert(query_result.getCount() == 1)
+          assert("Some Title".equals(query_result.getItems().get(0).get("Title").getS()))
+        }
+        it ("from table using BETWEEN") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("BETWEEN"), Some("A"), Some("Z"))
+          assert(query_result.getCount() == 2)
+        }
+        it ("finds all values when using hash key only") {
+          val query_result = Table.query(range_table_name, "Id", "101")
+          assert(query_result.getCount() == 2)
+        }
+      }
+
+      it ("should delete range table") {
+        try {
+          Table.delete(range_table_name)
+        }
+        catch {
+          case e: ResourceNotFoundException => {}
+        }
+        await atMost(2, MINUTES) until {
+          var exception:Throwable = null
+          try {
+            Table.describe(range_table_name)
+          }
+          catch {
+            case e: Throwable => {
+              exception = e
+            }
+          }
+          exception != null
+        }
+      }
+  }
+
+}
