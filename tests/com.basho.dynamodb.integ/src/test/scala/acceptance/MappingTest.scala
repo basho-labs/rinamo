d1bd2c8778de8eaf46591f5fd07da870698b4954
Randy Secrist <randy.secrist@gmail.com>
2014-05-09 07:37:38 -0600

    more mapping coverage

--- tests/com.basho.dynamodb.integ/src/test/scala/acceptance/MappingTest.scala
+++ tests/com.basho.dynamodb.integ/src/test/scala/acceptance/MappingTest.scala
@@ -19,31 +19,29 @@ class HashKeyTest extends FunSpec
   with BeforeAndAfterEach with AwaitilitySupport {
 
   val hash_table_name = "test_table_hash"
-  var table:CreateTableResult = null
-  var item:Item = null
+  var table_hash:CreateTableResult = null
+  val item_1 = new Item(
+    ("Id", "N", "101"),
+    ("Title", "S", "Some Title"),
+    ("ISBN", "S", "ABC"))
+  val item_2 = new Item(
+    ("Id", "N", "102"),
+    ("Title", "S", "Another Title"),
+    ("ISBN", "S", "DEF"))
+  val item_3 = new Item(
+    ("Id", "N", "101"),
+    ("Title", "S", "Tale of Two Databases"),
+    ("ISBN", "S", "XYZ"))
 
   override def beforeEach() {
-    table = Table.create(hash_table_name, "Id", "N")
+    table_hash = Table.create(hash_table_name, "Id", "N")
     await atMost(2, MINUTES) until {
       val result = Table.describe(hash_table_name)
       // TODO:  and these together, once rinamo goes async
       "ACTIVE".equals(result.getTable().getTableStatus())
       hash_table_name.equals(result.getTable().getTableName())
     }
-    item = new Item(
-        ("Id", "N", "101"),
-        ("Title", "S", "Some Title"),
-        ("ISBN", "S", "ABC"))
-    val item2 = new Item(
-        ("Id", "N", "102"),
-        ("Title", "S", "Another Title"),
-        ("ISBN", "S", "DEF"))
-    val item3 = new Item(
-        ("Id", "N", "101"),
-        ("Title", "S", "Tale of Two Databases"),
-        ("ISBN", "S", "XYZ"))
-
-    Table.put(hash_table_name)(item, item2, item3)
+    Table.put(hash_table_name)(item_1, item_2, item_3)
   }
   override def afterEach() {
     try {
@@ -69,7 +67,7 @@ class HashKeyTest extends FunSpec
   describe ("test uses dynamodb api, table using hash key") {
     describe ("US193704: manage tables, create table") {
       it ("should create table") {
-        assert(hash_table_name.equals(table.getTableDescription().getTableName()))
+        assert(hash_table_name.equals(table_hash.getTableDescription().getTableName()))
       }
 
       it ("should fail creating an existing table") {
@@ -110,7 +108,7 @@ class HashKeyTest extends FunSpec
     }
     describe ("US193713: modify data, put item") {
       it ("should put item in table") {
-        val result = Table.put(hash_table_name, item)
+        val result = Table.put(hash_table_name, item_1)
         assert("{}".equals(result.toString()))
       }
 
@@ -120,26 +118,26 @@ class HashKeyTest extends FunSpec
               new AttributeValue("Bar")).withExists(true)
           var expected:Map[String, ExpectedAttributeValue] = Map()
           expected += (("Foo", invalid_expect))
-          Table.put(hash_table_name, item, Some(expected), None)
+          Table.put(hash_table_name, item_1, Some(expected), None)
         } should produce [ConditionalCheckFailedException]
       }
 
       it ("should fail put if using non existing table") {
         evaluating {
-          Table.put("nonexistant_table", item)
+          Table.put("nonexistant_table", item_1)
         } should produce [ResourceNotFoundException]
       }
 
       // ALL_OLD appears to behave like NONE, but different from these
       it ("should fail put if using invalid return values") {
         evaluating {
-          Table.put(hash_table_name, item, None, Some(ReturnValue.ALL_NEW))
+          Table.put(hash_table_name, item_1, None, Some(ReturnValue.ALL_NEW))
         } should produce [AmazonServiceException]
         evaluating {
-          Table.put(hash_table_name, item, None, Some(ReturnValue.UPDATED_OLD))
+          Table.put(hash_table_name, item_1, None, Some(ReturnValue.UPDATED_OLD))
         } should produce [AmazonServiceException]
         evaluating {
-          Table.put(hash_table_name, item, None, Some(ReturnValue.UPDATED_NEW))
+          Table.put(hash_table_name, item_1, None, Some(ReturnValue.UPDATED_NEW))
         } should produce [AmazonServiceException]
       }
     }
@@ -167,15 +165,90 @@ class HashKeyTest extends FunSpec
     }
 
     describe ("[US193709, US193711]: table type, hash + range, LWW behavior") {
-      describe ("read data, query item") {
-        it ("from table using EQ") (pending)
-        it ("from table using LE") (pending)
-        it ("from table using LT") (pending)
-        it ("from table using GE") (pending)
-        it ("from table using GT") (pending)
-        it ("from table using BEGINS_WITH") (pending)
-        it ("from table using BETWEEN") (pending)
+      val range_table_name = "test_table_range"
+      it ("should create range table") {
+        val table_range:CreateTableResult = Table.create(range_table_name, "Id", "N", Some("Title"), Some("S"))
+        Table.put(range_table_name)(item_1, item_2, item_3)
+        await atMost(2, MINUTES) until {
+          val result = Table.describe(range_table_name)
+          // TODO:  and these together, once rinamo goes async
+          "ACTIVE".equals(result.getTable().getTableStatus())
+          range_table_name.equals(result.getTable().getTableName())
+        }
       }
+
+      describe ("read data, query item") {
+        it ("from table using EQ") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("EQ"), Some("Some Title"))
+          assert(query_result.getCount() == 1)
+          assert("Some Title".equals(query_result.getItems().get(0).get("Title").getS()))
+        }
+        it ("from table using LE") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("LE"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 2)
+        }
+        it ("from table using LT") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("LT"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 1)
+        }
+        it ("from table using GE") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("GE"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 1)
+        }
+        it ("from table using GT") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("GT"), Some("Tale of Two Databases"))
+          assert(query_result.getCount() == 0)
+        }
+        it ("from table using BEGINS_WITH") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("BEGINS_WITH"), Some("Some"))
+          assert(query_result.getCount() == 1)
+          assert("Some Title".equals(query_result.getItems().get(0).get("Title").getS()))
+        }
+        it ("from table using BETWEEN") {
+          val query_result = Table.query(
+              range_table_name, "Id", "101",
+              Some("Title"), Some("BETWEEN"), Some("A"), Some("Z"))
+          assert(query_result.getCount() == 2)
+        }
+        it ("finds all values when using hash key only") {
+          val query_result = Table.query(range_table_name, "Id", "101")
+          assert(query_result.getCount() == 2)
+        }
+      }
+
+      it ("should delete range table") {
+        try {
+          Table.delete(range_table_name)
+        }
+        catch {
+          case e: ResourceNotFoundException => {}
+        }
+        await atMost(2, MINUTES) until {
+          var exception:Throwable = null
+          try {
+            Table.describe(range_table_name)
+          }
+          catch {
+            case e: Throwable => {
+              exception = e
+            }
+          }
+          exception != null
+        }
+      }
+
     }
 
     describe ("US193712: read data, scan item") {
