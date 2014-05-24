549ca344206adccaa89ed0f541e0e92208f8fae3
Randy Secrist <randy.secrist@gmail.com>
2014-05-23 19:38:31 -0600

    extract auth handler logic, note cowboy irregularity

--- src/rinamo_middleware_auth.erl
+++ src/rinamo_middleware_auth.erl
@@ -22,19 +22,13 @@ execute(Req, Env) ->
         _ ->
             case cowboy_req:method(Req) of
                 {<<"POST">>, _} ->
-                    AccessKey = tokenize_auth_header(binary_to_list(AuthToken)),
-
-                    {_, {_, HandlerOpts}, PartialEnv} = lists:keytake(handler_opts, 1, Env),
-                    lager:debug("Auth HandlerOpts: ~p~n", [HandlerOpts]),
-                    State = case HandlerOpts of
-                        [] -> #state{user_key = AccessKey};
-                        _ -> HandlerOpts#state{user_key = AccessKey}
-                    end,
-
-                    NewEnv = [{handler_opts, State} | PartialEnv],
-
-                    {ok, Req, NewEnv};
+                    execute_auth_handler(AuthToken, Req, Env);
+                {<<>>, _} ->
+                    % observed that cowboy does this *sometimes*
+                    % https://github.com/extend/cowboy/issues/448
+                    execute_auth_handler(AuthToken, Req, Env);
                 _ ->
+                    lager:debug("WTF: ~p~n", [cowboy_req:method(Req)]),
                     ErrorMsg = rinamo_error:make(incomplete_signature),
                     {_, NextReq} = rinamo_response:send(ErrorMsg#error.http_code, rinamo_error:format(ErrorMsg), Req),
                     {halt, NextReq}
@@ -42,6 +36,21 @@ execute(Req, Env) ->
     end.
 
 %% Internal
+execute_auth_handler(AuthToken, Req, Env) ->
+    AccessKey = tokenize_auth_header(binary_to_list(AuthToken)),
+
+    {_, {_, HandlerOpts}, PartialEnv} = lists:keytake(handler_opts, 1, Env),
+    lager:debug("Auth HandlerOpts: ~p~n", [HandlerOpts]),
+    State = case HandlerOpts of
+        [] -> #state{user_key = AccessKey};
+        _ -> HandlerOpts#state{user_key = AccessKey}
+    end,
+
+    NewEnv = [{handler_opts, State} | PartialEnv],
+
+    {ok, Req, NewEnv}.
+
+
 tokenize_auth_header(HeaderValue) ->
     case HeaderValue of
       undefined -> undefined;
