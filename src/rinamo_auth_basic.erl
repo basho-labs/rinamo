15d93907ab17a15dc8950b91df7c98dbadc3f78a
Randy Secrist <randy.secrist@gmail.com>
2014-06-07 11:11:03 -0600

    fix compile warnings

--- src/rinamo_auth_basic.erl
+++ src/rinamo_auth_basic.erl
@@ -7,5 +7,5 @@
 % In addition to be totally insecure, this
 % is really stupid for data sharing, so
 % please don't use this in production.
-authorize(AccessKey, Signature, Body, Req) ->
+authorize(AccessKey, _, _, _) ->
     AccessKey.
